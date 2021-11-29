0cae3bdfaa8db626e22cfcef47ad21d9e96f0549
Li Mo <bakule2012@hotmail.com>
2021-11-29 12:02:28 -0500

    Initial Commit

---
+++ FRCScotter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FRCScotter
+//
+//  Created by Jin Lin on 2021-11-29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
