250dd53dca75c4493ecbf5b10792929bda44493b
Li Mo <bakule2012@hotmail.com>
2021-12-04 18:31:55 -0500

    Add moving view above keyboard, insert and load data from Teams

--- FRCScouter/Controller/TeamsViewController.swift
+++ FRCScouter/Controller/TeamsViewController.swift
@@ -6,8 +6,16 @@
 //
 
 import UIKit
+import CoreData
 
-class TeamsController: UITableViewController {
+@available(iOS 13.0, *)
+
+class TeamsViewController: UITableViewController {
+    
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    var arrTeams = [Teams]()
     
     //floating button
     lazy var faButton: UIButton = {
@@ -28,6 +36,23 @@ class TeamsController: UITableViewController {
         // set navigationBar's title color
         let textAttributes = [NSAttributedString.Key.foregroundColor:UIColor.white]
         navigationController?.navigationBar.titleTextAttributes = textAttributes
+        
+        //notification keyboard will show
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(sender:)), name: UIResponder.keyboardWillShowNotification, object: nil);
+        
+        //notification keyboard will hide
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(sender:)), name: UIResponder.keyboardWillHideNotification, object: nil);
+        
+        //Load data from Teams
+        loadTeams()
+    }
+    
+    @objc func keyboardWillShow(sender: NSNotification) {
+         self.view.frame.origin.y = -200 // Move view 150 points upward
+    }
+
+    @objc func keyboardWillHide(sender: NSNotification) {
+         self.view.frame.origin.y = 0 // Move view to original position
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -62,15 +87,25 @@ class TeamsController: UITableViewController {
     //Press on floating button
     @objc func fabTapped(_ button: UIButton) {
         print("button tapped")
-        var team_number: Int = 0
+        var team_number: String = ""
         
         var textField = UITextField()
         let alert = UIAlertController(title: "Add Teams", message: "", preferredStyle: .alert)
         let action = UIAlertAction(title: "Add", style: .default) { (action) in
             //what will happen once the user clicks the Add button on our UIAlert
-            if let No = Int(textField.text!) {
-                team_number = No
-                self.getJasonDataFromTBA()
+            if let s = textField.text {
+               team_number = s
+                //self.getJasonDataFromTBA()
+                self.getJasonDataFromTBA(teamField: "team", teamName: team_number) { result in
+                    guard let teamsItemModel = result else { return }
+               //print("Team Number = \(String(describing: teamsItemModel.team_number))")
+               //print("School Name = \(String(describing: teamsItemModel.school_name))")
+                    let newTeams = Teams(context: self.context)
+                    newTeams.team_no = Int64(teamsItemModel.team_number!)
+                    newTeams.team_name = teamsItemModel.school_name
+                    self.arrTeams.append(newTeams)
+                    self.saveTeams()
+                }
             }
         }
         alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
@@ -84,9 +119,10 @@ class TeamsController: UITableViewController {
     }
     
     // Get Jason Data from The Blue Alliance
-    func getJasonDataFromTBA(){
+    func getJasonDataFromTBA(teamField: String, teamName: String, completion: @escaping (TeamsResponseModel?) -> Void){
         // Create URL
-        let url = URL(string: "https://www.thebluealliance.com/api/v3/team/frc7520")
+        let s = "https://www.thebluealliance.com/api/v3/" + teamField + "/frc" + teamName
+        let url = URL(string: s)
         guard let requestUrl = url else { fatalError() }
         // Create URL Request
         var request = URLRequest(url: requestUrl)
@@ -98,14 +134,16 @@ class TeamsController: UITableViewController {
         let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
             
             // Check if Error took place
-            guard let data = data else { return }
+            guard let data = data else {
+                completion(nil)
+                return
+                
+            }
+                
                    // Using parseJSON() function to convert data to Swift struct
             let teamsItem = self.parseJSON(data: data)
-                       
-                   // Read Teams item team_number & school_name
-                   guard let teamsItemModel = teamsItem else { return }
-            print("Team Number = \(String(describing: teamsItemModel.team_number))")
-            print("School Name = \(String(describing: teamsItemModel.school_name))")
+                completion(teamsItem)
+                   
         }
         task.resume()
  
@@ -124,9 +162,46 @@ class TeamsController: UITableViewController {
         return returnValue
     }
     
+    //MARK: - Data Manipulation Methods
+    
+    func saveTeams(){
+        do {
+            try context.save()
+        } catch {
+            print("Error saving context \(error)")
+        }
+        DispatchQueue.main.async {
+            self.tableView.reloadData()
+        }
+    }
+    
+    func loadTeams() {
+        let request: NSFetchRequest<Teams> = Teams.fetchRequest()
+        do {
+            arrTeams = try context.fetch(request)
+        } catch {
+            print("Error fetching data from context \(error)")
+        }
+        
+        tableView.reloadData()
+        
+    }
+    
     //MARK: - TableView dataSource
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return arrTeams.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "teamsCell", for: indexPath) as! CustomTableViewCell
+        cell.lblTeamNo.text = String(arrTeams[indexPath.row].team_no)
+        cell.lblTeamName?.text = arrTeams[indexPath.row].team_name
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 96.0
     }
 
 }
